18631642af7b1b61560c45a7c2b92cdd22165334
Ivan Savkov <ivansavkov89@gmail.com>
2026-03-30 10:25:48 +0300

    Initial Commit

---
+++ KOLATA.eu/KOLATA_euApp.swift
@@ -0,0 +1,17 @@
+//
+//  KOLATA_euApp.swift
+//  KOLATA.eu
+//
+//  Created by Ivan Savkov on 30.03.26.
+//
+
+import SwiftUI
+
+@main
+struct KOLATA_euApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
